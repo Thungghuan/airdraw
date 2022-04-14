9fa37a6dc9cfa0c7177c862171308ac05351e502
Thungghuan <1092887426@qq.com>
2022-04-14 20:30:19 +0800

    feat: add display page

--- plugin/index.ts
+++ plugin/index.ts
@@ -4,10 +4,8 @@ export default function (): PluginOption {
   return {
     name: 'airdraw:communication',
     configureServer(server) {
-      server.ws.on('airdraw:draw-moving', (data: any) => {
-        server.config.logger.info(
-          `update points: ${JSON.stringify(data.points)}`
-        )
+      server.ws.on('airdraw:draw-moving', ({ points }) => {
+        server.ws.send('airdraw:display', { points })
       })
     }
   }
--- src/pages/display.vue
+++ src/pages/display.vue
@@ -1,5 +1,28 @@
+<script setup lang="ts">
+import { ref, onMounted } from 'vue'
+import { drawOnCanvas } from '../utils'
+
+const canvasRef = ref<HTMLCanvasElement>()
+let canvasContext: CanvasRenderingContext2D | undefined
+
+onMounted(() => {
+  const canvasEl = canvasRef.value!
+  canvasEl.width = window.innerWidth * 2
+  canvasEl.height = window.innerHeight * 2
+
+  canvasContext = canvasEl.getContext('2d')!
+})
+
+if (import.meta.hot) {
+  import.meta.hot.on('airdraw:display', ({ points }) => {
+    drawOnCanvas(canvasContext!, points)
+  })
+}
+</script>
+
 <template>
-  <div font-mono>
-    <h1>Diplay</h1>
+  <div w-screen h-screen bg-gray font-mono select-none>
+    <h1 absolute m0 left-5 top-3 text-gray-700>AirDraw - Display</h1>
+    <canvas w-full h-full ref="canvasRef" />
   </div>
 </template>
